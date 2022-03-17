0a3bd3b70c0193e58dd8647e045833491e9b3b52
Reisuke1231 <reisuke1231@gmail.com>
2022-03-17 14:17:02 +0900

    Configure STATIC_ROOT

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from job import views as job_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', job_views.home, name='home'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
